12b9d0b2fc9cce4b0593879977f3f43109516ea6
asacrea <csanch3595@gmail.com>
2020-09-02 23:12:23 -0500

    Modifiación método processRead

--- src/uniandes/algorithms/readsanalyzer/KmersTable.java
+++ src/uniandes/algorithms/readsanalyzer/KmersTable.java
@@ -1,20 +1,25 @@
 package uniandes.algorithms.readsanalyzer;
 
+import java.util.HashMap;
 import java.util.Set;
 
 import ngsep.sequences.RawRead;
+import uniandes.algobc.structures.Gene;
 /**
  * Stores abundances information on a list of subsequences of a fixed length k (k-mers)
  * @author Jorge Duitama
  */
 public class KmersTable implements RawReadProcessor {
 
+	private int kmerSize;
+	private HashMap<String,Integer> kMerSizes = new HashMap<>();
 	/**
 	 * Creates a new table with the given k-mer size
 	 * @param kmerSize length of k-mers stored in this table
 	 */
 	public KmersTable(int kmerSize) {
 		// TODO: Implementar metodo
+		this.kmerSize = kmerSize;
 	}
 
 	/**
@@ -23,7 +28,26 @@ public class KmersTable implements RawReadProcessor {
 	 */
 	public void processRead(RawRead read) {
 		String sequence = read.getSequenceString();
-		// TODO Implementar metodo. Calcular todos los k-mers del tamanho dado en la constructora y actualizar la abundancia de cada k-mer
+		// TODO Implementar metodo. Calcular todos los k-mers del tamanho dado en la constructora
+		// y actualizar la abundancia de cada k-mer
+		//Search all posible kMers
+		for(int x=0; x < sequence.length()-this.kmerSize; x++) {
+			String sub1 = sequence.substring(x, x+this.kmerSize);
+			if(this.kMerSizes.containsKey(sub1)) {
+				continue;
+			}else {
+				int count = 1;
+				//Search how many fixed kMer sequence have
+				for(int y = x+1; y < sequence.length()-this.kmerSize; y++) {
+					String sub2 = sequence.substring(y, y+this.kmerSize);
+					if( sub1 == sub2) {
+						count++;
+					}
+				}
+				//Add new kMer to dictionary
+				this.kMerSizes.put(sub1, count);
+			}
+		}
 		
 	}
 	
