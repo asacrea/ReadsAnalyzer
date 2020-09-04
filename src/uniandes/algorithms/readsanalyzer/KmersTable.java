823f219a550e610df387d1f5bc9cf3bee079c08e
asacrea <csanch3595@gmail.com>
2020-09-03 21:57:46 -0500

    Coding calculateAbundancesDistribution Method (Incomplete)

--- src/uniandes/algorithms/readsanalyzer/KmersTable.java
+++ src/uniandes/algorithms/readsanalyzer/KmersTable.java
@@ -13,13 +13,14 @@ public class KmersTable implements RawReadProcessor {
 
 	private int kmerSize;
 	private HashMap<String,Integer> kMerSizes = new HashMap<>();
+	private HashMap<Integer,Integer> distribution = new HashMap<>();
 	/**
 	 * Creates a new table with the given k-mer size
 	 * @param kmerSize length of k-mers stored in this table
 	 */
 	public KmersTable(int kmerSize) {
 		// TODO: Implementar metodo
-		this.kmerSize = kmerSize;
+		this.kmerSize = kmerSize; 
 	}
 
 	/**
@@ -57,7 +58,9 @@ public class KmersTable implements RawReadProcessor {
 	 */
 	public Set<String> getDistinctKmers() {
 		// TODO Implementar metodo
-		return null;
+		Set<String> keyset = kMerSizes.keySet();
+		
+		return keyset;
 	}
 	
 	/**
@@ -67,16 +70,41 @@ public class KmersTable implements RawReadProcessor {
 	 */
 	public int getAbundance(String kmer) {
 		// TODO Implementar metodo
-		return 0;
+		int a = 1;
+		return kMerSizes.get(kmer);
 	}
 	
 	/**
+	 * K-Mers abundance distribution
 	 * Calculates the distribution of abundances
 	 * @return int [] array where the indexes are abundances and the values are the number of k-mers
 	 * observed as many times as the corresponding array index. Position zero should be equal to zero
 	 */
-	public int[] calculateAbundancesDistribution() {
+	public HashMap calculateAbundancesDistribution() {
 		// TODO Implementar metodo
-		return null;
+		HashMap<String, Integer> kMerSizesCopy = kMerSizes;
+		
+		for(String x: kMerSizes.keySet()) {
+			int abundance = kMerSizes.get(x);
+			if(this.distribution.containsKey(x)) {
+				continue;
+			}else {
+				int count = 1;
+				//Search how many fixed kMer sequence have
+				for(String y: kMerSizes.keySet()) {
+					if(this.distribution.containsKey(x)) {
+						continue;
+					}else {
+						int abundance_2 = kMerSizes.get(x);
+						if( abundance == abundance_2) {
+							count++;
+						}
+					}
+				}
+				//Add new kMer to dictionary
+				this.distribution.put(abundance, count); 
+			}
+		}
+		return distribution;
 	}
 }
